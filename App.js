6dac4f94b9d5a5675d79422fc654357ca6a501cf
Jahnavi.B <139149353+jahnavibalabomma@users.noreply.github.com>
2023-09-19 16:12:08 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ExoticCocktailsPage from "./PagesOfZomato/ExoticCocktailsPage";
+
+function App() {
+  return (
+    <div>
+      <ExoticCocktailsPage></ExoticCocktailsPage>
+    </div>
+  );
+}
+
+export default App;
